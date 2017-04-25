91cf40b19c525ffe887d3e2d67f85d1543066f5a
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2017-04-25 08:13:00 +0200

    bugfix(*): lint.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     rules: {
         "block-no-empty": true,
         "color-hex-case": "lower",
-        "max-nesting-depth": 3,
+        "max-nesting-depth": 5,
         "max-empty-lines": 1,
         "unit-case": "lower",
         "selector-pseudo-element-case": "lower",
