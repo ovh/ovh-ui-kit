1b5f1a95709e8e23a425d96e299ed5fb1898b4f1
NicoPennec <nemohp@yahoo.fr>
2017-06-02 10:56:33 +0200

    feat(oui-icons): generate icons indexes

---
+++ scripts/generate-icons-indexes.js
@@ -0,0 +1,34 @@
+'strict mode';
+
+const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
+const icons = require('../dist/icons/icons.json');
+const log = console.log;
+
+log(chalk.yellow(`\nGenerating less variables from icons`));
+
+// init less file
+let file = fs.createWriteStream('packages/oui-icons/_icons.less');
+
+log(chalk.blue(`Generated`), chalk.cyan(path.resolve(file.path)));
+
+let iconsIndexes = [`@oui-icon-index:`];
+let iconsCodes = [`\n`];
+
+Object.keys(icons).forEach((key) => {
+    let value = icons[key];
+    let index = key.substr(5);
+    iconsIndexes.push(`  ~"${index}"`);
+    iconsCodes.push(`@oui-icon-index-${index}: "${value}";`)
+});
+
+iconsIndexes.push(`;`);
+
+file.write(`\n// WARNING: THIS FILE IS GENERATED, PLEASE DO NOT EDIT IT!\n\n`);
+file.write(iconsIndexes.join(`\n`));
+file.write(iconsCodes.join(`\n`));
+
+file.end();
+
+log(chalk.green(`Done`));
