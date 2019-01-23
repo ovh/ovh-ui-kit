6c9d8623cab7a6768cb7bcf32263edd5146a84f1
Axel Peter <axel.peter@corp.ovh.com>
2019-01-23 17:12:13 +0100

    fix: fix eslint and stylint

--- packages/apps/ui-kit/scripts/generate-icons-indexes.js
+++ packages/apps/ui-kit/scripts/generate-icons-indexes.js
@@ -1,38 +1,41 @@
-'strict mode';
+"strict mode";
 
-const chalk = require('chalk');
-const fs = require('fs');
-const path = require('path');
-const icons = require('../dist/fonts/oui-icons/oui-icons.json');
+const chalk = require("chalk");
+const fs = require("fs");
+const path = require("path");
+const icons = require("../dist/fonts/oui-icons/oui-icons.json");
 const log = console.log;
 
-log(chalk.yellow(`\nGenerating less variables from icons`));
+log(chalk.yellow("\nGenerating less variables from icons"));
 
 // init less file
-let file = fs.createWriteStream('../../components/_core/less/_icons.less');
+const file = fs.createWriteStream("../../components/_core/less/_icons.less");
 
-log(chalk.blue(`Generated`), chalk.cyan(path.resolve(file.path)));
+log(chalk.blue("Generated"), chalk.cyan(path.resolve(file.path)));
 
-let iconsIndexes = [`@oui-icon-index:`];
-let iconsCodes = [`\n`];
+const iconsIndexes = ["@oui-icon-index:"];
+const iconsCodes = ["\n"];
 
 Object.keys(icons).forEach((key) => {
-    let value = icons[key];
-    let index = key.substr(5);
+    const value = icons[key];
+    const prefix = "icon-";
+
+    // Remove prefix from key name
+    const index = key.substr(prefix.length);
     iconsIndexes.push(`  ~"${index}"`);
-    iconsCodes.push(`@oui-icon-index-${index}: "${value}";`)
+    iconsCodes.push(`@oui-icon-index-${index}: "${value}";`);
 });
 
 const lastIndex = iconsIndexes.length;
-iconsIndexes[lastIndex - 1] += ';';
+iconsIndexes[lastIndex - 1] += ";";
 
-file.write(`// Icons\n`);
-file.write(`//\n`);
-file.write(`// WARNING: THIS FILE IS GENERATED, PLEASE DO NOT EDIT IT!\n`);
-file.write(iconsIndexes.join(`\n`));
-file.write(iconsCodes.join(`\n`));
-file.write(`\n`);
+file.write("// Icons\n");
+file.write("//\n");
+file.write("// WARNING: THIS FILE IS GENERATED, PLEASE DO NOT EDIT IT!\n");
+file.write(iconsIndexes.join("\n"));
+file.write(iconsCodes.join("\n"));
+file.write("\n");
 
 file.end();
 
-log(chalk.green(`Done`));
+log(chalk.green("Done"));
