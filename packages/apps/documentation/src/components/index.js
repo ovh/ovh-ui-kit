f70a0e854b1d027674fded0e1b729544c0004334
Axel Peter <axel.peter@corp.ovh.com>
2019-02-13 16:58:02 +0100

    feat(documentation): add recursive sidebar-menu

--- packages/apps/documentation/src/components/index.js
+++ packages/apps/documentation/src/components/index.js
@@ -1,10 +1,12 @@
 import Showcase from "./showcase";
+import SidebarMenu from "./sidebar-menu";
 
 const moduleName = "documentation.components";
 
 angular
     .module(moduleName, [
-        Showcase
+        Showcase,
+        SidebarMenu
     ]);
 
 export default moduleName;
