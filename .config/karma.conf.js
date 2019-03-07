85ee966bad770a389b41695148044e6d3bd03096
Axel Peter <axel.peter@live.com>
2019-03-07 14:12:42 +0100

    build: move dependencies to core

--- .config/karma.conf.js
+++
@@ -1,9 +0,0 @@
-const karmaConfig = require("@ovh/ui-kit.core/src/config/karma.base.config");
-
-module.exports = function (config) {
-  const files = 'packages/components/**/*.spec.js';
-  karmaConfig.files.push(files);
-  karmaConfig.preprocessors[files] = ['webpack', 'sourcemap'];
-
-  config.set(karmaConfig);
-};
