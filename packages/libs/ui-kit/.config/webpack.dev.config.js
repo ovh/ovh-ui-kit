b4b69955859bf6fb41993c8a3bb56530fad66e06
Axel Peter <axel.peter@live.com>
2019-03-08 11:39:02 +0100

    refactor: fix missing and looping dependencies

--- packages/libs/ui-kit/.config/webpack.dev.config.js
+++ packages/libs/ui-kit/.config/webpack.dev.config.js
@@ -3,22 +3,22 @@ const path = require("path");
 const merge = require("webpack-merge");
 
 module.exports = merge(baseConfig, {
-    mode: "development",
-    devtool: "source-map",
-    output: {
-        path: path.resolve(".", "dist", "js"),
-        filename: "oui.js",
-        chunkFilename: "[id].[chunkhash].js"
-    },
-    externals: {
-        angular: "require('angular')",
-        "angular-aria": "require('angular-aria')",
-        "angular-sanitize": "require('angular-sanitize')",
-        "bloodhound-js": "require('bloodhound-js')",
-        clipboard: "require('clipboard')",
-        flatpickr: "require('flatpickr')",
-        "popper.js": "require('popper.js')",
-        "ui-select": "require('ui-select')"
-    },
-    watch: true
+  mode: "development",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(".", "dist", "js"),
+    filename: "oui.js",
+    chunkFilename: "[id].[chunkhash].js"
+  },
+  externals: {
+    angular: "require('angular')",
+    "angular-aria": "require('angular-aria')",
+    "angular-sanitize": "require('angular-sanitize')",
+    "bloodhound-js": "require('bloodhound-js')",
+    clipboard: "require('clipboard')",
+    flatpickr: "require('flatpickr')",
+    "popper.js": "require('popper.js')",
+    "ui-select": "require('ui-select')"
+  },
+  watch: true
 });
--- packages/libs/ui-kit/.config/webpack.dist.config.js
+++ packages/libs/ui-kit/.config/webpack.dist.config.js
@@ -4,26 +4,26 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = merge(baseConfig, {
-    mode: "production",
-    devtool: "source-map",
-    output: {
-        path: path.resolve(".", "dist", "js"),
-        filename: "oui.js"
-    },
-    externals: {
-        angular: "require('angular')",
-        "angular-aria": "require('angular-aria')",
-        "angular-sanitize": "require('angular-sanitize')",
-        "bloodhound-js": "require('bloodhound-js')",
-        clipboard: "require('clipboard')",
-        flatpickr: "require('flatpickr')",
-        "popper.js": "require('popper.js')",
-        "ui-select": "require('ui-select')"
-    },
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin() // Enable scope hoisting
-    ],
-    performance: {
-        hints: false
-    }
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(".", "dist", "js"),
+    filename: "oui.js"
+  },
+  externals: {
+    angular: "require('angular')",
+    "angular-aria": "require('angular-aria')",
+    "angular-sanitize": "require('angular-sanitize')",
+    "bloodhound-js": "require('bloodhound-js')",
+    clipboard: "require('clipboard')",
+    flatpickr: "require('flatpickr')",
+    "popper.js": "require('popper.js')",
+    "ui-select": "require('ui-select')"
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin() // Enable scope hoisting
+  ],
+  performance: {
+    hints: false
+  }
 });
