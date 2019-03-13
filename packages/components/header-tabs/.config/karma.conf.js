8d01a62f3877786da6c95cbc1478a63009c49a98
Axel Peter <axel.peter@live.com>
2019-03-13 13:31:06 +0100

    refactor: move header-tabs to header

--- packages/components/header-tabs/.config/karma.conf.js
+++
@@ -1,9 +0,0 @@
-const karmaConfig = require("@ovh/ui-kit.core/src/config/karma.base.config");
-
-module.exports = function (config) {
-  const files = 'src/**/*.spec.js';
-  karmaConfig.files.push(files);
-  karmaConfig.preprocessors[files] = ['webpack', 'sourcemap'];
-
-  config.set(karmaConfig);
-};
--- packages/components/header/src/js/index.js
+++ packages/components/header/src/js/index.js
@@ -1,10 +1,13 @@
 import angular from 'angular';
 import Header from './header.component';
+import HeaderTabs from './tabs/index';
 
 const moduleName = 'oui.header';
 
 angular
-  .module(moduleName, [])
+  .module(moduleName, [
+    HeaderTabs,
+  ])
   .component('ouiHeader', Header);
 
 export default moduleName;
