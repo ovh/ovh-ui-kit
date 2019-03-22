363ddfda1b4b1fc3ba783733605937d5434bcd39
Jérôme Martin <euhmeuh@users.noreply.github.com>
2019-03-22 14:47:41 +0100

    docs: start writing actual stories for button calendar clipboard modal (#355)

--- packages/apps/workshop/stories/calendar.stories.js
+++ packages/apps/workshop/stories/calendar.stories.js
@@ -5,31 +5,20 @@ import { forModule } from 'storybook-addon-angularjs';
 import Calendar from '@ovh/ui-kit.calendar';
 import '@ovh/ui-kit.calendar/src/index.less';
 
-storiesOf('Components/Calendar', module)
+storiesOf('Molecules/Calendar', module)
   .add(
-    'default',
-    forModule(Calendar).createElement(compile => compile`
-      <oui-calendar model="$ctrl.model"></oui-calendar>
-    `),
-  )
-  .add(
-    'placeholder',
-    forModule(Calendar).createElement(compile => compile`
-      <oui-calendar model="$ctrl.model" placeholder="YYYY-MM-DD"></oui-calendar>
-    `),
-  )
-  .add(
-    'disabled',
+    'Simple date selector',
     forModule(Calendar).createElement((compile) => {
-      const disabled = boolean('disabled', true);
-
+      const disabled = boolean('Disabled', false);
       return compile`
-        <oui-calendar model="$ctrl.model" disabled="${disabled}"></oui-calendar>
+        <oui-calendar model="$ctrl.model"
+                      placeholder="YYYY-MM-DD"
+                      disabled="${disabled}"></oui-calendar>
       `;
     }),
   )
   .add(
-    'inline',
+    'Full calendar in-place',
     forModule(Calendar).createElement(compile => compile`
       <oui-calendar model="$ctrl.model" inline></oui-calendar>
     `),
---
+++ packages/apps/workshop/stories/modal/modal.stories.js
@@ -0,0 +1,35 @@
+import { storiesOf } from '@storybook/html';
+import { forModule } from 'storybook-addon-angularjs';
+
+import Module from './index';
+
+storiesOf('Organisms/Modal', module)
+  .add(
+    'Open a modal',
+    forModule(Module).createElement(compile => compile`
+      <oui-modal heading="Settings for MyServer01"
+                primary-label="Save"
+                primary-action="$ctrl.confirm = true"
+                secondary-label="Cancel"
+                secondary-action="$ctrl.cancel = true"
+                on-dismiss="$ctrl.cancel = true">
+        <form novalidate name="serverAccessForm">
+          <oui-field label="Server name" size="xl" error-messages="{
+              pattern: 'Name must be alphabetic with a length between 3 and 8.'
+            }">
+            <input class="oui-input"
+                  type="text"
+                  placeholder="Name of the server"
+                  name="name"
+                  ng-model="$ctrl.name"
+                  ng-pattern="/^[a-zA-Z]{3,8}$/"
+                  required>
+          </oui-field>
+          <oui-field label="Server password" size="xl">
+            <oui-password placeholder="Password to access the server"
+                          model="$ctrl.password">
+          </oui-field>
+        </form>
+      </oui-modal>
+    `),
+  );
