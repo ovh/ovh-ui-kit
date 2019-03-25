3faf98f641fde0a2a67cfbe73674a25b326d6f49
Axel Peter <axel.peter@live.com>
2019-03-25 14:38:10 +0100

    build: fix storybook substring error

--- packages/apps/workshop/.storybook/webpack.config.js
+++ packages/apps/workshop/.storybook/webpack.config.js
@@ -3,40 +3,39 @@ const RemcalcPlugin = require('less-plugin-remcalc');
 
 const exclude = [/node_modules(?![/\\](@ovh))/, /dist/];
 
-module.exports = {
-  module: {
-    rules: [{
-      test: /\.stories\.jsx?$/,
-      loaders: [require.resolve('@storybook/addon-storysource/loader')],
-      enforce: 'pre',
-    }, {
-      test: /\.css|.less$/,
-      use: [
-        {
-          loader: 'style-loader',
+module.exports = async ({config }) => {
+  // For @storybook/addon-storysource
+  config.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre',
+  });
+
+  // For LESS files
+  config.module.rules.push({
+    test: /\.css|.less$/,
+    use: [
+      {
+        loader: 'style-loader',
+      },
+      { loader: 'css-loader' },
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: () => [autoprefixer({ browsers: ['last 2 versions', 'ie 11'] })],
         },
-        { loader: 'css-loader', options: { sourceMap: true } },
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            plugins: () => [autoprefixer({ browsers: ['last 2 versions', 'ie 11'] })],
-          },
+      },
+      {
+        loader: 'less-loader',
+        options: {
+          plugins: [RemcalcPlugin],
         },
-        {
-          loader: 'less-loader',
-          options: {
-            plugins: [RemcalcPlugin],
-            sourceMap: true,
-          },
-        },
-      ],
-      exclude,
-    }],
-  },
-  resolve: {
-    alias: {
-      angular: require.resolve('angular'),
-    },
-  },
+      },
+    ],
+    exclude,
+  });
+
+  config.resolve.alias.angular = require.resolve('angular');
+
+  return config;
 };
