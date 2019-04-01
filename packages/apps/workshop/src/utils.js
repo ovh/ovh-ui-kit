29a080839291c79532ec7b767830b79c6ea18c0c
Jérôme Martin <jerome.martin@corp.ovh.com>
2019-04-01 14:41:29 +0200

    fix: stories update

--- packages/apps/workshop/src/utils.js
+++ packages/apps/workshop/src/utils.js
@@ -2,27 +2,37 @@ import styles from './styles';
 
 const wrapperId = 'centered-layout-wrapper';
 
-function getOrCreateWrapper(story) {
+function getOrCreateWrapper() {
   let wrapper = document.getElementById(wrapperId);
 
-  if(wrapper) {
+  if (wrapper) {
     return wrapper;
   }
 
   wrapper = document.createElement('div');
   wrapper.setAttribute('id', wrapperId);
   Object.assign(wrapper.style, styles.wrapper);
+
+  return wrapper;
+}
+
+function replaceStory(wrapper, story) {
   const inner = document.createElement('div');
   Object.assign(inner.style, styles.inner);
+
+  while (wrapper.firstChild) {
+    wrapper.removeChild(wrapper.firstChild);
+  }
+
   wrapper.appendChild(inner);
   inner.appendChild(story);
-
   return wrapper;
 }
 
 export function withCenteredLayout(getStory, context) {
   const story = getStory(context);
-  return getOrCreateWrapper(story);
+  const wrapper = getOrCreateWrapper();
+  return replaceStory(wrapper, story);
 }
 
 export function compileTemplate(template, scope = {}) {
