9f0184444ca5f09fb2fbe29ae66e94862c08e4fc
Jérôme Martin <euhmeuh@users.noreply.github.com>
2019-04-01 11:21:21 +0200

    docs: fix knobs in the centered layout wrapper (#366)

--- packages/apps/workshop/src/utils.js
+++ packages/apps/workshop/src/utils.js
@@ -1,20 +1,35 @@
 import styles from './styles';
 
-export function withCenteredLayout(getStory, context) {
-  const story = getStory(context);
-  const wrapper = document.createElement('div');
-  Object.assign(wrapper.style, styles.wrapper);
+const wrapperId = 'centered-layout-wrapper';
 
+function getOrCreateWrapper(story) {
+  let wrapper = document.getElementById(wrapperId);
+
+  if(wrapper) {
+    return wrapper;
+  }
+
+  wrapper = document.createElement('div');
+  wrapper.setAttribute('id', wrapperId);
+  Object.assign(wrapper.style, styles.wrapper);
   const inner = document.createElement('div');
   Object.assign(inner.style, styles.inner);
-
   wrapper.appendChild(inner);
   inner.appendChild(story);
+
   return wrapper;
+}
+
+export function withCenteredLayout(getStory, context) {
+  const story = getStory(context);
+  return getOrCreateWrapper(story);
 }
 
 export function compileTemplate(template, scope = {}) {
   return { template, scope };
 }
 
-export default { compileTemplate };
+export default {
+  compileTemplate,
+  withCenteredLayout,
+};
