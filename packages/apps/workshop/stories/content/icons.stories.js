021bda2b7b272046533d7b8a00a823033a26825f
Axel Peter <axel.peter@live.com>
2019-04-25 15:03:10 +0200

    docs: update content stories

--- packages/apps/workshop/stories/content/icons.stories.js
+++ packages/apps/workshop/stories/content/icons.stories.js
@@ -4,11 +4,24 @@ storiesOf('Content/Icons', module)
   .add(
     'Usage',
     () => `
-    <span class="oui-icon oui-icon-error_circle"></span>
-    <span class="oui-icon oui-icon-info_circle"></span>
-    <span class="oui-icon oui-icon-success_circle"></span>
-    <span class="oui-icon oui-icon-warning_circle"></span>
-    `,
+    <p>
+      <span class="oui-icon oui-icon-error_circle"></span>
+      <span class="oui-icon oui-icon-info_circle"></span>
+      <span class="oui-icon oui-icon-success_circle"></span>
+      <span class="oui-icon oui-icon-warning_circle"></span>
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span class="oui-icon oui-icon-error_circle"></span><br />
+      Duis semper ligula nec fringilla tempor. In rhoncus ullamcorper feugiat. <span class="oui-icon oui-icon-info_circle"></span><br />
+      Phasellus vel ipsum vitae neque varius luctus. Proin id iaculis arcu. <span class="oui-icon oui-icon-success_circle"></span><br />
+      Fusce justo arcu, egestas vel nulla nec, dictum cursus lacus. <span class="oui-icon oui-icon-warning_circle"></span>
+    </p>
+    `, {
+      options: {
+        showPanel: true,
+      },
+    },
   )
   .add(
     'Basic',
