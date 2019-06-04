f0069a3fc5832b65064dcb99ba8250114e51d3e1
Axel Peter <axel.peter@live.com>
2019-06-04 15:20:06 +0200

    fix(button): fix icon placement with full width

--- packages/apps/workshop/stories/styles/button.stories.js
+++ packages/apps/workshop/stories/styles/button.stories.js
@@ -11,38 +11,6 @@ storiesOf('Styles/Button', module)
     `,
   )
   .add(
-    'Full width',
-    () => `
-    <button class="oui-button oui-button_primary oui-button_full-width">I take a looooooooot of spaaaaaace!</button>
-    `,
-  )
-  .add(
-    'Large height',
-    () => `
-    <button class="oui-button oui-button_primary oui-button_large-height">I'm so high</button>
-    <button class="oui-button oui-button_secondary oui-button_large-height">
-      <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
-      Back
-    </button>
-    <button class="oui-button oui-button_secondary oui-button_large-height">
-      Next
-      <span class="oui-icon oui-icon-chevron-right" aria-hidden="true"></span>
-    </button>
-    `,
-  )
-  .add(
-    'Small width',
-    () => `
-    <button class="oui-button oui-button_primary oui-button_small-width">Ok</button>
-    <button class="oui-button oui-button_secondary oui-button_small-width">
-      <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
-    </button>
-    <button class="oui-button oui-button_secondary oui-button_small-width">
-      <span class="oui-icon oui-icon-chevron-right" aria-hidden="true"></span>
-    </button>
-    `,
-  )
-  .add(
     'With arrows',
     () => `
     <button class="oui-button oui-button_primary oui-button_icon-left">
@@ -78,6 +46,67 @@ storiesOf('Styles/Button', module)
     () => `
     <button type="button" class="oui-button oui-button_icon-alone">
       <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
+    </button>
+    `,
+  )
+  .add(
+    'Full width',
+    () => `
+    <div style="width: 40%; display: inline-block;">
+      <button class="oui-button oui-button_full-width oui-button_primary">Primary Button Full width</button>
+      <button class="oui-button oui-button_full-width oui-button_link oui-button_icon-left">
+          <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
+          Button link Full width Icon left
+      </button>
+      <button class="oui-button oui-button_full-width oui-button_icon-left oui-button_primary">
+          <span class="oui-icon oui-icon_circle oui-icon-chevron-left" aria-hidden="true"></span>
+          Primary Full width Icon left
+      </button>
+      <button class="oui-button oui-button_full-width oui-button_icon-left oui-button_secondary">
+          <span class="oui-icon oui-icon_circle oui-icon-chevron-left" aria-hidden="true"></span>
+          Secondary Full width Icon left
+      </button>
+    </div>
+    <div style="width: 40%; display: inline-block;">
+      <button class="oui-button oui-button_full-width oui-button_secondary">Secondary Button Full width</button>
+      <button class="oui-button oui-button_full-width oui-button_link oui-button_icon-right">
+          Button link Full width Icon right
+          <span class="oui-icon oui-icon-chevron-right" aria-hidden="true"></span>
+      </button>
+      <button class="oui-button oui-button_full-width oui-button_icon-right oui-button_primary">
+          Secondary Full width Icon right
+          <span class="oui-icon oui-icon_circle oui-icon-chevron-right" aria-hidden="true"></span>
+      </button>
+      <button class="oui-button oui-button_full-width oui-button_icon-right oui-button_secondary">
+          Secondary Full width Icon right
+          <span class="oui-icon oui-icon_circle oui-icon-chevron-right" aria-hidden="true"></span>
+      </button>
+    </div>
+    `,
+  )
+  .add(
+    'Large height',
+    () => `
+    <button class="oui-button oui-button_primary oui-button_large-height">I'm so high</button>
+    <button class="oui-button oui-button_secondary oui-button_large-height">
+      <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
+      Back
+    </button>
+    <button class="oui-button oui-button_secondary oui-button_large-height">
+      Next
+      <span class="oui-icon oui-icon-chevron-right" aria-hidden="true"></span>
+    </button>
+    `,
+  )
+  .add(
+    'Small width',
+    () => `
+    <button class="oui-button oui-button_primary oui-button_small-width">Ok</button>
+    <button class="oui-button oui-button_secondary oui-button_small-width">
+      <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
+    </button>
+    <button class="oui-button oui-button_secondary oui-button_small-width">
+      <span class="oui-icon oui-icon-chevron-right" aria-hidden="true"></span>
     </button>
     `,
   );
