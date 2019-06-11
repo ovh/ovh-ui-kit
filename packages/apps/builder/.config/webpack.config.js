a99a3e40b2af04bc48f0a21de5ad076d7dda42de
Axel Peter <15101925+AxelPeter@users.noreply.github.com>
2019-06-11 16:27:56 +0200

    build: remove useless duplicate js file in the dist (#443)

--- packages/apps/builder/.config/webpack.config.js
+++ packages/apps/builder/.config/webpack.config.js
@@ -2,21 +2,31 @@ const baseConfig = require('@ovh-ux/ui-kit.config/src/webpack.dist.config');
 const merge = require('webpack-merge');
 const path = require('path');
 
+const ExtraneousFileCleanupPlugin = require('webpack-extraneous-file-cleanup-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(baseConfig, {
   entry: {
     'oui': './src/index.js',
-    'oui-b10': './src/base-10.js',
+
+    // Themes
+    'oui-b10': './src/_base-10.less',
   },
   output: {
     path: path.resolve('.', 'dist', 'js'),
     filename: '[name].js',
   },
   plugins: [
+    // Extract CSS to separate files
     new MiniCssExtractPlugin({
       filename: '../css/[name].css',
-      allChunks: true,
+    }),
+
+    // Remove unwanted JS files created by Webpack
+    // When creating an entry for CSS only (known bug)
+    // https://github.com/webpack/webpack/issues/1967
+    new ExtraneousFileCleanupPlugin({
+      extensions: ['.js'],
     }),
   ],
   externals: {
--- packages/apps/builder/src/base-10.js
+++
@@ -1,4 +0,0 @@
-import './base-10.less'; // Styles
-import moduleName from './js/components'; // Components
-
-export default moduleName;
