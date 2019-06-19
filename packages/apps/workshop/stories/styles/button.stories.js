ab69b0868120aba739ffa8bfa97707775b81f8ee
Axel Peter <axel.peter@live.com>
2019-06-19 14:42:20 +0200

    feat(ods): add style for button

--- packages/apps/workshop/stories/styles/button.stories.js
+++ packages/apps/workshop/stories/styles/button.stories.js
@@ -4,43 +4,74 @@ storiesOf('Styles/Button', module)
   .add(
     'With colors',
     () => `
-    <button class="oui-button">Default</button>
-    <button class="oui-button" disabled>Default disabled</button>
-    <button class="oui-button oui-button_primary">Primary</button>
-    <button class="oui-button oui-button_primary" disabled>Primary disabled</button>
-    <button class="oui-button oui-button_secondary">Secondary</button>
-    <button class="oui-button oui-button_secondary" disabled>Secondary disabled</button>
+    <div>
+      <button class="oui-button oui-button_primary">Primary</button>
+      <button class="oui-button oui-button_secondary">Secondary</button>
+    </div>
+    <div>
+      <button class="oui-button oui-button_primary" disabled>Primary</button>
+      <button class="oui-button oui-button_secondary" disabled>Secondary</button>
+    </div>
     `,
   )
   .add(
     'With arrows',
     () => `
-    <button class="oui-button oui-button_primary oui-button_icon-left">
-      <span class="oui-icon oui-icon_circle oui-icon-chevron-left" aria-hidden="true"></span>
-      Point your finger to the left!
-    </button>
-    <button class="oui-button oui-button_secondary oui-button_icon-right">
-      Point your finger to the right!
-      <span class="oui-icon oui-icon_circle oui-icon-chevron-right" aria-hidden="true"></span>
-    </button>
-    <button class="oui-button oui-button_dropdown">
-      I have something to hide...
-      <span class="oui-icon oui-icon-chevron-down" aria-hidden="true"></span>
-    </button>
+    <div>
+      <button class="oui-button oui-button_primary oui-button_icon-left">
+        <span class="oui-icon oui-icon_circle oui-icon-chevron-left" aria-hidden="true"></span>
+        Point your finger to the left!
+      </button>
+      <button class="oui-button oui-button_secondary oui-button_icon-right">
+        Point your finger to the right!
+        <span class="oui-icon oui-icon_circle oui-icon-chevron-right" aria-hidden="true"></span>
+      </button>
+      <button class="oui-button oui-button_dropdown">
+        I have something to hide...
+        <span class="oui-icon oui-icon-chevron-down" aria-hidden="true"></span>
+      </button>
+    </div>
+    <div>
+      <button class="oui-button oui-button_primary oui-button_icon-left" disabled>
+        <span class="oui-icon oui-icon_circle oui-icon-chevron-left" aria-hidden="true"></span>
+        Point your finger to the left!
+      </button>
+      <button class="oui-button oui-button_secondary oui-button_icon-right" disabled>
+        Point your finger to the right!
+        <span class="oui-icon oui-icon_circle oui-icon-chevron-right" aria-hidden="true"></span>
+      </button>
+      <button class="oui-button oui-button_dropdown" disabled>
+        I have something to hide...
+        <span class="oui-icon oui-icon-chevron-down" aria-hidden="true"></span>
+      </button>
+    </div>
     `,
   )
   .add(
     'As links',
     () => `
-    <button class="oui-button oui-button_link">Click me to get more information</button>
-    <button class="oui-button oui-button_link oui-button_icon-left">
-      <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
-      Previous page
-    </button>
-    <button class="oui-button oui-button_link oui-button_icon-right">
-      Next page
-      <span class="oui-icon oui-icon-chevron-right" aria-hidden="true"></span>
-    </button>
+    <div>
+      <button class="oui-button oui-button_link">Click me to get more information</button>
+      <button class="oui-button oui-button_link oui-button_icon-left">
+        <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
+        Previous page
+      </button>
+      <button class="oui-button oui-button_link oui-button_icon-right">
+        Next page
+        <span class="oui-icon oui-icon-chevron-right" aria-hidden="true"></span>
+      </button>
+    </div>
+    <div>
+      <button class="oui-button oui-button_link" disabled>Click me to get more information</button>
+      <button class="oui-button oui-button_link oui-button_icon-left" disabled>
+        <span class="oui-icon oui-icon-chevron-left" aria-hidden="true"></span>
+        Previous page
+      </button>
+      <button class="oui-button oui-button_link oui-button_icon-right" disabled>
+        Next page
+        <span class="oui-icon oui-icon-chevron-right" aria-hidden="true"></span>
+      </button>
+    </div>
     `,
   )
   .add(
