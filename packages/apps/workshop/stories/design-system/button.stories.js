2c59e0f110fc2a9263265138431fc95aaf067c6e
Axel Peter <axel.peter@live.com>
2019-07-02 15:23:43 +0200

    feat(button): update primary style for ODS theme

---
+++ packages/apps/workshop/stories/design-system/button.stories.js
@@ -0,0 +1,63 @@
+import { storiesOf } from '@storybook/html';
+
+storiesOf('Components|Buttons/Primary', module)
+  .add(
+    'Normal',
+    () => `
+    <button class="oui-button oui-button_primary">
+      Call to action
+    </button>`,
+  )
+  .add(
+    'Normal (Disabled)',
+    () => `
+    <button class="oui-button oui-button_primary" disabled>
+      Call to action
+    </button>`,
+  )
+  .add(
+    'Normal + Icon',
+    () => `
+    <button class="oui-button oui-button_primary oui-button_icon-left">
+      <span class="oui-icon oui-icon-folder_extra-thin" aria-hidden="true"></span>
+      Call to action
+    </button>`,
+  )
+  .add(
+    'Normal + Icon (Disabled)',
+    () => `
+    <button class="oui-button oui-button_primary oui-button_icon-left" disabled>
+      <span class="oui-icon oui-icon-folder_extra-thin" aria-hidden="true"></span>
+      Call to action
+    </button>`,
+  )
+  .add(
+    'Small',
+    () => `
+    <button class="oui-button oui-button_s oui-button_primary">
+      Call to action
+    </button>`,
+  )
+  .add(
+    'Small (Disabled)',
+    () => `
+    <button class="oui-button oui-button_s oui-button_primary" disabled>
+      Call to action
+    </button>`,
+  )
+  .add(
+    'Small + Icon',
+    () => `
+    <button class="oui-button oui-button_s oui-button_primary oui-button_icon-left">
+      <span class="oui-icon oui-icon-folder_extra-thin" aria-hidden="true"></span>
+      Call to action
+    </button>`,
+  )
+  .add(
+    'Small + Icon (Disabled)',
+    () => `
+    <button class="oui-button oui-button_s oui-button_primary oui-button_icon-left" disabled>
+      <span class="oui-icon oui-icon-folder_extra-thin" aria-hidden="true"></span>
+      Call to action
+    </button>`,
+  );
