fc876072123dcc46de2c673af12a4aba8ac7edac
Jérémy DE CESARE <jeremy.de-cesare@ext.ovh.com>
2019-07-08 17:18:52 +0200

    fix(oui-datagrid): reflect scope change on row detail item

--- packages/components/datagrid/src/js/row-detail/datagrid-row-detail.controller.js
+++ packages/components/datagrid/src/js/row-detail/datagrid-row-detail.controller.js
@@ -12,10 +12,17 @@ export default class {
 
   compileElement() {
     this.rowDetailScope.$row = this.row;
+    this.copyRow = angular.copy(this.row);
 
     this.datagridCtrl.rowDetailCompiledTemplate(this.rowDetailScope, (clone) => {
       this.$element.empty();
       this.$element.append(clone);
     });
+  }
+
+  $doCheck() {
+    if (!angular.equals(this.row, this.copyRow)) {
+      this.$postLink();
+    }
   }
 }
