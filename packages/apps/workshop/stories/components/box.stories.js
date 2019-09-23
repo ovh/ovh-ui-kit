48a79bc4621aa634a586b09f44e9efc71156e427
feoche <francois.eoche@corp.ovh.com>
2019-09-23 16:30:26 +0200

    fix(box): add title for heading instead of generic div

--- packages/apps/workshop/stories/components/box.stories.js
+++ packages/apps/workshop/stories/components/box.stories.js
@@ -5,8 +5,8 @@ storiesOf('Components|Box/Native', module)
     'Default',
     () => `
     <div class="oui-box">
-      <div class="oui-box__heading">An awesome title for an awesome box</div>
-      <div class="oui-box__body">
+      <h4 class="oui-box__heading">An awesome title for an awesome box</h4>
+      <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Sed tempus sapien erat, et molestie magna tristique non.
         Sed tincidunt eu augue tempor consectetur. Nullam iaculis,
@@ -16,7 +16,7 @@ storiesOf('Components|Box/Native', module)
         ultricies lacus in, fermentum nisi. Cras sed neque id dui
         fermentum faucibus. Mauris mollis arcu justo, eget convallis
         massa volutpat ac.
-      </div>
+      </p>
     </div>
     `,
   )
@@ -24,8 +24,8 @@ storiesOf('Components|Box/Native', module)
     'Light',
     () => `
     <div class="oui-box oui-box_light">
-      <div class="oui-box__heading">An awesome title for an awesome box</div>
-      <div class="oui-box__body">
+      <h4 class="oui-box__heading">An awesome title for an awesome box</h4>
+      <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Sed tempus sapien erat, et molestie magna tristique non.
         Sed tincidunt eu augue tempor consectetur. Nullam iaculis,
@@ -35,7 +35,7 @@ storiesOf('Components|Box/Native', module)
         ultricies lacus in, fermentum nisi. Cras sed neque id dui
         fermentum faucibus. Mauris mollis arcu justo, eget convallis
         massa volutpat ac.
-      </div>
+      </p>
     </div>
     `,
   );
