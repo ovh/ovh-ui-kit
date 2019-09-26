9932bcb8a06011b48121e0cbc306820e086b6641
Axel Peter <axel.peter@live.com>
2019-09-26 16:50:49 +0200

    feat(collapsible): apply ODS theme

---
+++ packages/apps/workshop/stories/components/collapsible.stories.js
@@ -0,0 +1,98 @@
+import { storiesOf } from '@storybook/html';
+import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
+import { forModule } from 'storybook-addon-angularjs';
+
+import { compileTemplate } from '../../src/utils';
+
+// Create mock module for the stories
+const moduleName = 'oui-collapsible-stories';
+angular.module(moduleName, ['oui.collapsible']);
+
+storiesOf('Components|Collapsible/WebComponent', module)
+  .add(
+    'Default',
+    forModule(moduleName).createElement(() => compileTemplate(`
+    <oui-collapsible
+      heading="Title"
+      aria-label="Action"
+      expanded="$ctrl.expanded"
+      on-toggle="$ctrl.onToggle(expanded)">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Duis semper ligula nec fringilla tempor. In rhoncus
+      ullamcorper feugiat. Phasellus vel ipsum vitae neque
+      varius luctus. Proin id iaculis arcu. Fusce justo arcu,
+      egestas vel nulla nec, dictum cursus lacus. Aenean
+      elementum vel odio quis rutrum. In quis tellus in neque
+      vulputate rhoncus vitae ut justo. Ut dignissim varius est
+      in consequat. Donec nisi mauris, pellentesque condimentum
+      congue in, blandit ut arcu. In et elit ipsum.
+    </oui-collapsible>
+    `, {
+      $ctrl: {
+        expanded: boolean('Expanded state', false),
+        onToggle: action('onToggle'),
+      },
+    })),
+  )
+  .add(
+    'Multiple',
+    forModule(moduleName).createElement(() => compileTemplate(`
+    <oui-collapsible
+      heading="Title">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Duis semper ligula nec fringilla tempor. In rhoncus
+      ullamcorper feugiat. Phasellus vel ipsum vitae neque
+      varius luctus. Proin id iaculis arcu. Fusce justo arcu,
+      egestas vel nulla nec, dictum cursus lacus. Aenean
+      elementum vel odio quis rutrum. In quis tellus in neque
+      vulputate rhoncus vitae ut justo. Ut dignissim varius est
+      in consequat. Donec nisi mauris, pellentesque condimentum
+      congue in, blandit ut arcu. In et elit ipsum.
+    </oui-collapsible>
+
+    <oui-collapsible
+      heading="Title">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Duis semper ligula nec fringilla tempor. In rhoncus
+      ullamcorper feugiat. Phasellus vel ipsum vitae neque
+      varius luctus. Proin id iaculis arcu. Fusce justo arcu,
+      egestas vel nulla nec, dictum cursus lacus. Aenean
+      elementum vel odio quis rutrum. In quis tellus in neque
+      vulputate rhoncus vitae ut justo. Ut dignissim varius est
+      in consequat. Donec nisi mauris, pellentesque condimentum
+      congue in, blandit ut arcu. In et elit ipsum.
+    </oui-collapsible>
+
+    <oui-collapsible
+      heading="Title">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Duis semper ligula nec fringilla tempor. In rhoncus
+      ullamcorper feugiat. Phasellus vel ipsum vitae neque
+      varius luctus. Proin id iaculis arcu. Fusce justo arcu,
+      egestas vel nulla nec, dictum cursus lacus. Aenean
+      elementum vel odio quis rutrum. In quis tellus in neque
+      vulputate rhoncus vitae ut justo. Ut dignissim varius est
+      in consequat. Donec nisi mauris, pellentesque condimentum
+      congue in, blandit ut arcu. In et elit ipsum.
+    </oui-collapsible>
+
+    <oui-collapsible
+      heading="Title">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Duis semper ligula nec fringilla tempor. In rhoncus
+      ullamcorper feugiat. Phasellus vel ipsum vitae neque
+      varius luctus. Proin id iaculis arcu. Fusce justo arcu,
+      egestas vel nulla nec, dictum cursus lacus. Aenean
+      elementum vel odio quis rutrum. In quis tellus in neque
+      vulputate rhoncus vitae ut justo. Ut dignissim varius est
+      in consequat. Donec nisi mauris, pellentesque condimentum
+      congue in, blandit ut arcu. In et elit ipsum.
+    </oui-collapsible>
+    `, {
+      $ctrl: {
+        expanded: boolean('Expanded state', false),
+        onToggle: action('onToggle'),
+      },
+    })),
+  );
