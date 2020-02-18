d40b979e9a91b4c5763f970bd00a410ed0f68f1b
Axel Peter <axel.peter@live.com>
2020-02-18 17:23:31 +0100

    docs(helpers): update helpers documentation
    Signed-off-by: Axel Peter <axel.peter@live.com>

---
+++ packages/apps/workshop/stories/v4/helpers/backgrounds.stories.js
@@ -0,0 +1,325 @@
+export default {
+  title: 'Version 4/Helpers/Backgrounds',
+  parameters: {
+    options: {
+      showPanel: false,
+    },
+  },
+};
+
+export const Primary = () => `
+<div class="doc-badges">
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-000"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-000</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-050"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-050</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-075"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-075</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-100"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-200"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-200</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-300"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-400"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-500"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-500</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-600"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-600</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-700"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-700</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-800"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-800</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-900"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-900</code></div>
+  </div>
+
+</div>
+
+<!-- Primary (Text) -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-p-800-text"></div>
+      <div class="doc-badge__code"><code>.oui-background-p-800-text</code></div>
+  </div>
+
+</div>`;
+
+export const Validation = () => `
+<!-- Error -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-ae-100"></div>
+    <div class="doc-badge__code"><code>.oui-background-ae-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-ae-300"></div>
+    <div class="doc-badge__code"><code>.oui-background-ae-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-ae-400"></div>
+    <div class="doc-badge__code"><code>.oui-background-ae-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-ae-500"></div>
+    <div class="doc-badge__code"><code>.oui-background-ae-500</code></div>
+  </div>
+
+</div>
+
+<!-- Warning -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-aw-100"></div>
+    <div class="doc-badge__code"><code>.oui-background-aw-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-aw-300"></div>
+    <div class="doc-badge__code"><code>.oui-background-aw-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-aw-400"></div>
+    <div class="doc-badge__code"><code>.oui-background-aw-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-aw-500"></div>
+    <div class="doc-badge__code"><code>.oui-background-aw-500</code></div>
+  </div>
+
+</div>
+
+<!-- Success -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-as-100"></div>
+    <div class="doc-badge__code"><code>.oui-background-as-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-as-300"></div>
+    <div class="doc-badge__code"><code>.oui-background-as-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-as-400"></div>
+    <div class="doc-badge__code"><code>.oui-background-as-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-as-500"></div>
+    <div class="doc-badge__code"><code>.oui-background-as-500</code></div>
+  </div>
+
+</div>`;
+
+export const Promotion = () => `
+<!-- Promotion -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-promotion-300"></div>
+    <div class="doc-badge__code"><code>.oui-background-promotion-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-promotion-500"></div>
+    <div class="doc-badge__code"><code>.oui-background-promotion-500</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-promotion-700"></div>
+    <div class="doc-badge__code"><code>.oui-background-promotion-700</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-promotion-900"></div>
+    <div class="doc-badge__code"><code>.oui-background-promotion-900</code></div>
+  </div>
+
+</div>
+
+<!-- Promotion (Text) -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-promotion-500-text"></div>
+    <div class="doc-badge__code"><code>.oui-background-promotion-500-text</code></div>
+  </div>
+
+</div>`;
+
+export const Status = () => `
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-alpha"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-alpha</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-beta"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-beta</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-new"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-new</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-soon"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-soon</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-promotion"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-promotion</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-price-drop"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-price-drop</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-sold-out"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-sold-out</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-best-seller"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-best-seller</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-limited-edition"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-limited-edition</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-delivery-120-seconds"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-delivery-120-seconds</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-delivery-24-hours"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-delivery-24-hours</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-delivery-72-hours"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-delivery-72-hours</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview oui-background-status-delivery-10-days"></div>
+    <div class="doc-badge__code"><code>.oui-background-status-delivery-10-days</code></div>
+  </div>
+
+</div>`;
+
+export const Dark = () => `
+<div class="doc-badges">
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-000"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-000</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-050"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-050</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-100"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-200"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-200</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-300"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-400"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-500"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-500</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-600"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-600</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-700"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-700</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-800"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-800</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview oui-background-g-900"></div>
+      <div class="doc-badge__code"><code>.oui-background-g-900</code></div>
+  </div>
+
+</div>`;
---
+++ packages/apps/workshop/stories/v4/helpers/colors.stories.js
@@ -0,0 +1,325 @@
+export default {
+  title: 'Version 4/Helpers/Colors',
+  parameters: {
+    options: {
+      showPanel: false,
+    },
+  },
+};
+
+export const Primary = () => `
+<div class="doc-badges">
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-000">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-000</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-050">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-050</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-075">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-075</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-200</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-500</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-600</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-700</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-800</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-900</code></div>
+  </div>
+
+</div>
+
+<!-- Primary (Text) -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-p-800-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-p-800-text</code></div>
+  </div>
+
+</div>`;
+
+export const Validation = () => `
+<!-- Error -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-ae-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-ae-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-ae-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-ae-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-ae-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-ae-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-ae-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-ae-500</code></div>
+  </div>
+
+</div>
+
+<!-- Warning -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-aw-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-aw-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-aw-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-aw-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-aw-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-aw-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-aw-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-aw-500</code></div>
+  </div>
+
+</div>
+
+<!-- Success -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-as-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-as-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-as-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-as-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-as-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-as-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-as-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-as-500</code></div>
+  </div>
+
+</div>`;
+
+export const Promotion = () => `
+<!-- Promotion -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-promotion-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-promotion-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-promotion-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-promotion-500</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-promotion-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-promotion-700</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-promotion-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-promotion-900</code></div>
+  </div>
+
+</div>
+
+<!-- Promotion (Text) -->
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-promotion-500-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-promotion-500-text</code></div>
+  </div>
+
+</div>`;
+
+export const Status = () => `
+<div class="doc-badges">
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-alpha">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-alpha</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-beta">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-beta</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-new">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-new</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-soon">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-soon</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-promotion">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-promotion</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-price-drop">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-price-drop</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-sold-out">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-sold-out</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-best-seller">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-best-seller</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-limited-edition">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-limited-edition</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-delivery-120-seconds">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-delivery-120-seconds</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-delivery-24-hours">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-delivery-24-hours</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-delivery-72-hours">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-delivery-72-hours</code></div>
+  </div>
+
+  <div class="doc-badge">
+    <div class="doc-badge__preview"><span class="oui-color-status-delivery-10-days">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+    <div class="doc-badge__code"><code>.oui-color-status-delivery-10-days</code></div>
+  </div>
+
+</div>`;
+
+export const Dark = () => `
+<div class="doc-badges">
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-000">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-000</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-050">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-050</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-100</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-200</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-300</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-400</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-500</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-600</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-700</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-800</code></div>
+  </div>
+
+  <div class="doc-badge">
+      <div class="doc-badge__preview"><span class="oui-color-g-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span></div>
+      <div class="doc-badge__code"><code>.oui-color-g-900</code></div>
+  </div>
+
+</div>`;
