5e34d968870a2e9daa17a21f4b428b3dd0469fbb
Axel Peter <15101925+AxelPeter@users.noreply.github.com>
2020-03-20 10:55:44 +0100

    feat(tooltip): apply ODS theme (#583)
    * docs(tooltip): update documentation
    * feat(tooltip): apply ODS theme
    
    Signed-off-by: Axel Peter <axel.peter@live.com>

---
+++ packages/apps/workshop/stories/design-system/directives/tooltip.stories.js
@@ -0,0 +1,79 @@
+import { select, text } from '@storybook/addon-knobs';
+import { forModule } from 'storybook-addon-angularjs';
+
+import Tooltip from '@ovh-ux/ui-kit.tooltip';
+
+import readme from '@ovh-ux/ui-kit.tooltip/README.md';
+import { compileTemplate } from '../../../src/utils';
+
+const placement = {
+  label: 'Placement',
+  options: {
+    Default: '',
+    Top: 'top',
+    Right: 'right',
+    Left: 'left',
+    Bottom: 'bottom',
+  },
+  default: '',
+};
+
+const moduleName = 'tooltip.stories';
+angular.module(moduleName, [Tooltip]);
+
+export default {
+  title: 'Design System/Directives/Tooltip',
+
+  parameters: {
+    notes: readme,
+    options: {
+      showPanel: true,
+    },
+  },
+};
+
+export const OnButton = forModule(moduleName).createElement(
+  () => compileTemplate(
+    `
+    <div class="doc-centered">
+      <div class="doc-centered__content">
+        <button class="oui-button oui-button_primary"
+          oui-tooltip="${text('Text', 'Lorem ipsum')}"
+          oui-tooltip-placement="${select(placement.label, placement.options, placement.default)}">
+          focus or hover me
+        </button>
+      </div>
+    </div>`,
+  ),
+);
+
+export const OnInput = forModule(moduleName).createElement(
+  () => compileTemplate(
+    `
+    <div class="doc-centered">
+      <div class="doc-centered__content">
+        <input class="oui-input oui-input_inline"
+          placeholder="focus or hover me"
+          type="text"
+          oui-tooltip="${text('Text', 'Lorem ipsum')}"
+          oui-tooltip-placement="${select(placement.label, placement.options, placement.default)}"></textarea>
+      </div>
+    </div>`,
+  ),
+);
+
+export const OnLink = forModule(moduleName).createElement(
+  () => compileTemplate(
+    `
+    <div class="doc-centered">
+      <div class="doc-centered__content">
+        <a class="oui-link"
+          href="#"
+          oui-tooltip="${text('Text', 'Lorem ipsum')}"
+          oui-tooltip-placement="${select(placement.label, placement.options, placement.default)}">
+          focus or hover me
+        </a>
+      </div>
+    </div>`,
+  ),
+);
