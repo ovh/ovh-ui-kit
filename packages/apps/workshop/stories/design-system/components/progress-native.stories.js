19f17a5f6bd2ea0c6d7cc0c56f395b6a8405b7e5
Axel Peter <15101925+AxelPeter@users.noreply.github.com>
2020-03-20 10:51:27 +0100

    feat(progress): apply ODS theme (#588)
    * docs(progress): update documentation stories
    * feat(progress): apply ODS theme
    
    Signed-off-by: Axel Peter <axel.peter@live.com>

--- packages/apps/workshop/stories/design-system/components/progress-native.stories.js
+++ packages/apps/workshop/stories/design-system/components/progress-native.stories.js
@@ -1,5 +1,5 @@
 export default {
-  title: 'Legacy/Components/Progress/Native',
+  title: 'Design System/Components/Progress/Native',
 
   parameters: {
     options: {
@@ -8,8 +8,15 @@ export default {
   },
 };
 
-export const Native = () => `
+export const Default = () => `
+<!-- Info -->
 <progress class="oui-progress oui-progress_info" value="5" max="100"></progress>
+
+<!-- Success -->
 <progress class="oui-progress oui-progress_success" value="10" max="100"></progress>
+
+<!-- Warning -->
 <progress class="oui-progress oui-progress_warning" value="60" max="100"></progress>
+
+<!-- Error -->
 <progress class="oui-progress oui-progress_error" value="100" max="100"></progress>`;
