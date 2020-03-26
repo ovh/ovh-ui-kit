914ee744d4a71da8c4c0e0f8bfacc6ce24820b69
Axel Peter <axel.peter@live.com>
2020-03-26 17:34:00 +0100

    docs(pagination): update documentation stories
    Signed-off-by: Axel Peter <axel.peter@live.com>

---
+++ packages/apps/workshop/stories/design-system/components/pagination-webcomponent.stories.js
@@ -0,0 +1,63 @@
+import { action } from '@storybook/addon-actions';
+import { forModule } from 'storybook-addon-angularjs';
+import { number, select } from '@storybook/addon-knobs';
+
+import Pagination from '@ovh-ux/ui-kit.pagination';
+
+import readme from '@ovh-ux/ui-kit.pagination/README.md';
+import { compileTemplate } from '../../../src/utils';
+
+// Create mock module for the stories
+const moduleName = 'pagination-webcomponent.stories';
+angular.module(moduleName, [Pagination]);
+
+const mode = {
+  label: 'Mode',
+  options: {
+    Button: 'button',
+    Select: 'select',
+    Input: 'input',
+  },
+  default: '',
+};
+
+export default {
+  title: 'Design System/Components/Pagination/WebComponent',
+
+  parameters: {
+    notes: readme,
+    options: {
+      showPanel: true,
+    },
+  },
+};
+
+export const Default = forModule(moduleName).createElement(
+  () => compileTemplate(
+    `
+    <oui-pagination
+      current-offset="${number('Current offset', 1)}"
+      page-size="${number('Page size', 25)}"
+      total-items="${number('Total items', 100)}">
+    </oui-pagination>`,
+  ),
+);
+
+export const Modes = forModule(moduleName).createElement(
+  () => compileTemplate(
+    `
+    <oui-pagination
+      mode="${select(mode.label, mode.options, mode.default)}"
+      current-offset="$ctrl.offset"
+      total-items="$ctrl.totalItems"
+      on-change="$ctrl.onChange($event)">
+    </oui-pagination>`,
+    {
+      $ctrl: {
+        offset: number('Current offset', 1),
+        totalItems: number('Total items', 100),
+        onChange: action('onChange'),
+      },
+    },
+  ),
+);
