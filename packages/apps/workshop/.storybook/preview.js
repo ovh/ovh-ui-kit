d7360e03668fa3e6f6e05f922048f19824188d0c
Axel Peter <axel.peter@live.com>
2020-05-26 16:00:28 +0200

    docs(storybook): fix stories sorting
    Signed-off-by: Axel Peter <axel.peter@live.com>

--- packages/apps/workshop/.storybook/preview.js
+++ packages/apps/workshop/.storybook/preview.js
@@ -28,11 +28,19 @@ addParameters({
     storySort: ([,a], [,b]) => {
       if (a.kind === b.kind) return 0;
 
-      // Put Introduction files first
-      if (a.id.includes('-introduction-') && !b.id.includes('-introduction-')) return -1;
-      if (!a.id.includes('-introduction-') && b.id.includes('-introduction-')) return 1;
+      // Put first elements with lower subfolder
+      if (a.kind.split('/').length < b.kind.split('/').length) return -1;
+      if (a.kind.split('/').length > b.kind.split('/').length) return 1;
 
-      // Then put MDX files second
+      // Put Documentation group first
+      if (a.id.startsWith('documentation-') && !b.id.startsWith('documentation-')) return -1;
+      if (!a.id.startsWith('documentation-') && b.id.startsWith('documentation-')) return 1;
+
+      // The put Introduction files first
+      if (a.id.endsWith('-introduction--page') && !b.id.endsWith('-introduction--page')) return -1;
+      if (!a.id.endsWith('-introduction--page') && b.id.endsWith('-introduction--page')) return 1;
+
+      // Then put others MDX files after
       if (a.id.endsWith('--page') && !b.id.endsWith('--page')) return -1;
       if (!a.id.endsWith('--page') && b.id.endsWith('--page')) return 1;
 
