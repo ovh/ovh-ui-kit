77f6330f1ccc12d31eaea8a02e11b459e7ea57a0
Axel Peter <axel.peter@live.com>
2020-06-01 16:49:28 +0200

    fix(header-tabs): display button if no href
    Signed-off-by: Axel Peter <axel.peter@live.com>

--- packages/apps/workshop/stories/legacy/components/header/header-webcomponent.stories.js
+++ packages/apps/workshop/stories/legacy/components/header/header-webcomponent.stories.js
@@ -45,9 +45,11 @@ export const WithTabs = forModule(moduleName).createElement(
             <oui-header-tabs-divider></oui-header-tabs-divider>
             <oui-header-tabs-item href="#">Checkbox</oui-header-tabs-item>
             <oui-header-tabs-item href="#">Radio</oui-header-tabs-item>
+            <oui-header-tabs-item>Button</oui-header-tabs-item>
         </oui-header-tabs-dropdown>
         <oui-header-tabs-item href="#" disabled>Pagination</oui-header-tabs-item>
         <oui-header-tabs-item href="#">Datagrid</oui-header-tabs-item>
+        <oui-header-tabs-item>Button</oui-header-tabs-item>
       </oui-header-tabs>
     </oui-header>`,
   ),
