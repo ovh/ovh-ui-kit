56abcd0d764a7cc08c249c1810cbdcdc2da31192
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2023-06-29 15:55:01 +0200

    fix(dropdown): update popper modifiers option
    ref: DTRSD-115509
    
    Enforce dropdown not to overflow outside of boundaries
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/components/dropdown/src/js/dropdown.controller.js
+++ packages/components/dropdown/src/js/dropdown.controller.js
@@ -158,7 +158,7 @@ export default class {
         },
         preventOverflow: {
           boundariesElement: 'viewport',
-          escapeWithReference: true,
+          escapeWithReference: false,
         },
       },
     });
